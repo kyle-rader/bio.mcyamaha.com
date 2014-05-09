ca6bae65722523d14025b6dd75389b31a9d6c6c8
kyle-rader <kyle.rader@ieee.org>
2014-05-09 09:47:00 -0700

    Add page for raw data view.

---
+++ app/raw_data.php
@@ -0,0 +1,17 @@
+<?php 
+$title = <<<EOT
+			<h2>Raw HIV Data</h2>
+			<small>HIV Variations Project</small>
+EOT;
+print PageTitle($title);
+?>
+<div class="row">
+	<div class="large-12 columns">
+		<h3>HIV data from 1980 - 2014</h3>
+	</div>
+</div>
+<div class="row-wide">
+     <div class="raw-data">
+data coming soon...
+     </div>
+</div>

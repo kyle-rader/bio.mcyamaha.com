cb490b5e600a3e0fc249be17fbe58a64e271e9c6
kyle-rader <kyle.rader@ieee.org>
2014-05-16 07:14:44 -0700

    Get RAW data onto page.

--- app/raw_data.php
+++ app/raw_data.php
@@ -1,17 +1,38 @@
 <?php 
+include_once("../include/db.inc");
+include_once("../database/hiv_data.php");
+
 $title = <<<EOT
-			<h2>Raw HIV Data</h2>
+			<h2>Raw HIV Consensus Data</h2>
 			<small>HIV Variations Project</small>
 EOT;
 print PageTitle($title);
+
+$hiv_data = GetHIVData($mysqli);
+
 ?>
 <div class="row">
 	<div class="large-12 columns">
-		<h3>HIV data from 1980 - 2014</h3>
+		<h4>HIV 1 Subtype B (North America)</h4>
+		<small>Hold shift while scrolling to scroll through the sequence</small>
 	</div>
 </div>
+<br>
 <div class="row-wide">
      <div class="raw-data">
-data coming soon...
+       <?php
+	  if(count($hiv_data) > 0) {
+       ?>
+       <table>
+	 <thead><tr><th class="fixed-head">Description</th><th class="long">Sequence</th></tr></thead>
+	 <tbody>
+	   <?php
+	      foreach($hiv_data as $hiv) {
+	         print "<tr><td class=\"fixed-head\">{$hiv['description']}</td><td class=\"long\">{$hiv['sequence']}</td></tr>";
+	      }
+	   ?>
+	 </tbody>
+       </table>
+       <?php } else { print 'No Data Found!'; } ?>
      </div>
 </div>
---
+++ database/hiv_data.php
@@ -0,0 +1,27 @@
+<?php
+
+function GetHIVData($mysqli) 
+{
+	$data = Array();
+	$sql = <<<EOT
+SELECT * FROM sequence
+WHERE consensus = 1
+ORDER BY year DESC;
+EOT;
+
+	if($stmt = $mysqli->prepare($sql))
+	{
+		$stmt->execute();
+		$stmt->bind_result($id, $year, $consensus, $description, $sequence);
+		while($stmt->fetch())
+		{
+			$data[] = Array('id' => $id,
+				   'year' => $year,
+				   'description' => $description,
+				   'sequence' => $sequence);
+		}
+		$stmt->close();
+	}
+	return $data;
+}
+?>

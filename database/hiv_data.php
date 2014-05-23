1e32ae5e7cbebd1f74d2f320337443cb820f3de3
kyle-rader <kyle.rader@ieee.org>
2014-05-23 11:14:25 -0700

    Fix Raw Data page for using new schema

--- database/hiv_data.php
+++ database/hiv_data.php
@@ -12,7 +12,7 @@ EOT;
 	if($stmt = $mysqli->prepare($sql))
 	{
 		$stmt->execute();
-		$stmt->bind_result($id, $year, $consensus, $description, $sequence);
+		$stmt->bind_result($id, $year, $consensus, $threshold, $description, $sequence);
 		while($stmt->fetch())
 		{
 			$data[] = Array('id' => $id,

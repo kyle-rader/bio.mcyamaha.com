6e30dac18c0ad3a938a0e7e3023b93ac086a5c4e
kyle-rader <kyle.rader@ieee.org>
2014-05-29 11:52:28 -0700

    Fix raw data page.

--- database/hiv_data.php
+++ database/hiv_data.php
@@ -12,10 +12,11 @@ EOT;
 	if($stmt = $mysqli->prepare($sql))
 	{
 		$stmt->execute();
-		$stmt->bind_result($id, $year, $consensus, $threshold, $description, $sequence);
+		$stmt->bind_result($id, $subtype, $year, $consensus, $threshold, $description, $sequence);
 		while($stmt->fetch())
 		{
 			$data[] = Array('id' => $id,
+				   'subtype' => $subtype,
 				   'year' => $year,
 				   'description' => $description,
 				   'sequence' => $sequence);
